c94d24111eb61b0c78d40fe067ae77bfff6030ab
Kamran <kgali21@gmail.com>
2021-01-21 11:13:25 -0800

    merge conflict

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,13 @@ const Header = () => {
 
     return (
         <>
-        <h1>Micki XO</h1>
-        <div>
-            <img src='../../public/insta.png' alt='Instagram Link' />
-            <img src='../../public/spotify.png' alt='Spotify Link' />
-            <img src='../../public/youtube.png' alt='Youtube Link' />
-            <img src='../../public/soundcloud.png' alt='Soundcloud Link' />
-        </div>
+            <h1>Micki XO</h1>
+            <div>
+                <img src='../../public/insta.png' alt='Instagram Link' />
+                <img src='../../public/spotify.png' alt='Spotify Link' />
+                <img src='../../public/youtube.png' alt='Youtube Link' />
+                <img src='../../public/soundcloud.png' alt='Soundcloud Link' />
+            </div>
         </>
     )
 }
