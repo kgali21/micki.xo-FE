bc63bf083081887ea2c61511cc3d2c09b39d95b0
Kamran <kgali21@gmail.com>
2021-04-12 12:25:48 -0700

    added fonts to frontend of site

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -33,7 +33,7 @@ const Subscribe = () => {
 
   return (
     <section id='Subscribe' className={styles.Form}>
-      <h1>CONNECT WITH MICKI</h1>
+      <h1 className={styles.Connect}>CONNECT WITH MICKI</h1>
       <form onSubmit={handleSubmit}>
         <div className={styles.InnerForm}> 
           <input type='text' placeholder='Enter Email Address' className={styles.Text} onChange={handleChange} />
