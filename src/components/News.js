5153f26a14b11665e312de7311f7592545a2f696
Kamran <kgali21@gmail.com>
2021-04-27 11:09:38 -0700

    news component media styled for iphone 12 size

--- src/components/News.js
+++ src/components/News.js
@@ -5,6 +5,7 @@ import styles from './News.module.css';
 const News = () => {
 
   const title = '"Silence"';
+  const upperTitle = title.toUpperCase();
 
   return (
     <section id='New' className={styles.News}>
@@ -12,7 +13,7 @@ const News = () => {
       <section className={styles.NewsContainer}>
         <img src='mickisilence.png' alt='Micki XO' className={styles.MickiImg}/>
         <div className={styles.BioContainer}>
-          <h2 className={styles.Title}>{title}</h2>
+          <h2 className={styles.Title}>{upperTitle}</h2>
           <p className={styles.Bio}>
             A sister song to her debut single, {title} showcases the side of toxic relationships from a more vulnerable and very personal place - while still delivering emotion and a catchy chorus. Micki XO wrote the lyrics to reflect how he felt silenced during her own toxic relationship. Where she was isolated from friends/family and felt like she was drowning in her silence, unable to reach out to those who she had pushed away. All money made from this single will be given to House of Ruth, a non-profit in Portland, OR, that aids victims of domestic abuse.
           </p>
